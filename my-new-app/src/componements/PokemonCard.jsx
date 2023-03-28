65787e981b32cf7628fa685bac4dd80598ff742e
Eric Godefroy <ericgodefroy@outlook.fr>
2023-03-28 10:56:43 +0200

    mysterious mew

--- my-new-app/src/componements/PokemonCard.jsx
+++ my-new-app/src/componements/PokemonCard.jsx
@@ -1,9 +1,25 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
+
 
 function PokemonCard (){
+    let Pokemon = pokemonList[1]
     return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbizar" />
-        <figcaption>bulbasaur</figcaption>
+        {typeof Pokemon.imgSrc !== "undefined" ? <img src= {Pokemon.imgSrc}/> : <p>???</p>}
+        <figcaption>{Pokemon.name}</figcaption>
     </figure>
 };
+
+
 
 export default PokemonCard
