319f4e9d0777fe7a17fb1789e3e2c2d49b979d44
Eric Godefroy <ericgodefroy@outlook.fr>
2023-03-29 10:38:58 +0200

    Swipe du pokemon

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,24 +1,51 @@
 import MyTitle from "./MyTitle.jsx";
 import PokemonCard from "./componements/PokemonCard.jsx";
+import { useState } from "react";
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function App() {
-  return (
-    <div>
-      <PokemonCard name={pokemonList[0].name} src={pokemonList[0].imgSrc}/>
-    </div>
-  );
-}
+  const [count, setCount] = useState(0); // count = la valeur retrouné setCount = l'action appliqué dessus useState(0) = ici la valeur initale sera de 0
 
+  const next = () => {
+    setCount(count + 1) 
+  }
+
+  const previous = () =>{
+    setCount(count -1)
+  }
+
+  return (<div>
+        <div>
+            <PokemonCard name={pokemonList[count].name} src={pokemonList[count].imgSrc}/>
+        </div>
+      {count > 0 ? <button onClick={previous}>Previous</button> : <button>First Element</button>}
+      {count < pokemonList.length - 1 ? <button onClick={next}>Next</button> : <button>Last Element</button> }
+    </div>);
+}
 
 export default App;
